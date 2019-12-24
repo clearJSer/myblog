ec94c492ae766ab2fbaf746405eae0a0836fa349
liuliang01 <liuliang01@sogou-inc.com>
2019-12-24 11:22:53 +0800

    articles list and articles content

--- components/Header.js
+++ components/Header.js
@@ -1,37 +1,54 @@
-import React from 'react';
-import '../static/style/components/header.css'
-import {Row, Col, Menu, Icon} from 'antd'
+import React, { useState, useEffect } from 'react';
+import '../static/style/components/header.css';
+import { Row, Col, Menu, Icon } from 'antd';
 import { MenuItem } from 'rc-menu';
+import Router from 'next/router';
+import Link from 'next/link';
+import axios from 'axios';
+import servicePath from '../config/apiUrl';
 const Header = () => {
-	return (
-		<header className="header">
-			<Row type="flex" justify="center">
-				<Col xs={24} sm={24} md={10} lg={15} xl={12}>
-					<span className="logo">Fire1024.com</span>
-					<span className="logo-text">进击的程序员，一起成长</span>
-				</Col>
-				<Col xs={0} sm={0} md={14} lg={8} xl={6}>
-					<Menu mode="horizontal">
-						<MenuItem key="home">
-							<Icon type="home"/>
-							首页
-						</MenuItem>
-						<MenuItem key="code">
-							<Icon type="home"/>
-							技术
-						</MenuItem>
-						<MenuItem key="money">
-							<Icon type="home"/>
-							财经
-						</MenuItem>
-						<MenuItem key="think">
-							<Icon type="home"/>    
-							思考
-						</MenuItem>
-					</Menu>
-				</Col>
-			</Row>
-		</header>
-	)
-}
+  const [navArray, setNavArray] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = axios.get(servicePath.getTypeInfo).then(res => {
+        setNavArray(res.data.data);
+        return res.data.data;
+      });
+    };
+    fetchData();
+	}, []);
+	const handleClick = (e) => {
+		if(e.key == 0) {
+			Router.push('/')
+		}else {
+			Router.push('/list?id='+e.key)
+		}
+	}
+  return (
+    <header className="header">
+      <Row type="flex" justify="center">
+        <Col xs={24} sm={24} md={10} lg={15} xl={12}>
+          <span className="logo">Fire1024</span>
+          <span className="logo-text">进击的程序员</span>
+        </Col>
+        <Col xs={0} sm={0} md={14} lg={8} xl={6}>
+          <Menu mode="horizontal"  onClick={handleClick}>
+            <MenuItem key="0">
+              <Icon type="home" theme="twoTone"/>
+              首页
+            </MenuItem>
+            {navArray.map(item => {
+              return (
+                <Menu.Item key={item.id}>
+									<Icon type={item.icon} theme="twoTone" />
+                  {item.typeName}
+                </Menu.Item>
+              );
+            })}
+          </Menu>
+        </Col>
+      </Row>
+    </header>
+  );
+};
 export default Header;
--- pages/detailed.js
+++ pages/detailed.js
@@ -1,110 +1,110 @@
-import React, { useState } from 'react'
-import Head from 'next/head'
-import { Row, Col, Icon, Breadcrumb, Affix } from 'antd'
-import ReactMarkdown from 'react-markdown'
-import MarkNav from 'markdown-navbar';
-import 'markdown-navbar/dist/navbar.css';
-import Header from '../components/Header'
-import Author from '../components/Author'
-import Advert from '../components/Advert'
-import Footer from '../components/Footer'
-import '../static/style/pages/detailed.css'
+import React, { useState } from 'react';
+import Head from 'next/head';
+import { Row, Col, Icon, Breadcrumb, Affix } from 'antd';
+// import MarkNav from 'markdown-navbar';
+// import 'markdown-navbar/dist/navbar.css';
+import Header from '../components/Header';
+import axios from 'axios';
+import Author from '../components/Author';
+import Advert from '../components/Advert';
+import Footer from '../components/Footer';
+import '../static/style/pages/detailed.css';
 
-const Detailed = () => {
+import marked from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/monokai-sublime.css';
+import Tocify from '../components/tocify.tsx';
 
-	let markdown = '# P01:课程介绍和环境搭建\n' +
-		'[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-		'> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-		'**这是加粗的文字**\n\n' +
-		'*这是倾斜的文字*`\n\n' +
-		'***这是斜体加粗的文字***\n\n' +
-		'~~这是加删除线的文字~~ \n\n' +
-		'\`console.log(111)\` \n\n' +
-		'# p02:来个Hello World 初始Vue3.0\n' +
-		'> aaaaaaaaa\n' +
-		'>> bbbbbbbbb\n' +
-		'>>> cccccccccc\n' +
-		'***\n\n\n' +
-		'# p03:Vue3.0基础知识讲解\n' +
-		'> aaaaaaaaa\n' +
-		'>> bbbbbbbbb\n' +
-		'>>> cccccccccc\n\n' +
-		'# p04:Vue3.0基础知识讲解\n' +
-		'> aaaaaaaaa\n' +
-		'>> bbbbbbbbb\n' +
-		'>>> cccccccccc\n\n' +
-		'#5 p05:Vue3.0基础知识讲解\n' +
-		'> aaaaaaaaa\n' +
-		'>> bbbbbbbbb\n' +
-		'>>> cccccccccc\n\n' +
-		'# p06:Vue3.0基础知识讲解\n' +
-		'> aaaaaaaaa\n' +
-		'>> bbbbbbbbb\n' +
-		'>>> cccccccccc\n\n' +
-		'# p07:Vue3.0基础知识讲解\n' +
-		'> aaaaaaaaa\n' +
-		'>> bbbbbbbbb\n' +
-		'>>> cccccccccc\n\n' +
-		'``` var a=11; ```'
-	return (
-		<>
-			<Head>
-				<title>博客详细页</title>
-			</Head>
-			<Header />
-			<Row className="comm-main" type="flex" justify="center">
-				<Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
-					<div>
-						<div className="bread-div">
-							<Breadcrumb>
-								<Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
-								<Breadcrumb.Item>视频列表</Breadcrumb.Item>
-								<Breadcrumb.Item>xxxx</Breadcrumb.Item>
-							</Breadcrumb>
-						</div>
+import servicePath from '../config/apiUrl';
 
-						<div>
-							<div className="detailed-title">
-								React实战视频教程-(更新08集)
-                </div>
+const Detailed = props => {
+  const tocify = new Tocify();
+  const renderer = new marked.Renderer();
 
-							<div className="list-icon center">
-								<span><Icon type="calendar" /> 2019-06-28</span>
-								<span><Icon type="folder" /> 视频教程</span>
-								<span><Icon type="fire" /> 5498人</span>
-							</div>
+  renderer.heading = function(text, level, raw) {
+    const anchor = tocify.add(text, level);
+    return `<a id="${anchor}" href="#${anchor}" class="anchor-fix"><h${level}>${text}</h${level}></a>\n`;
+  };
 
-							<div className="detailed-content" >
-								<ReactMarkdown
-									source={markdown}
-									escapeHtml={false}
-								/>
-							</div>
+  marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function(code) {
+      return hljs.highlightAuto(code).value;
+    },
+  });
+  let html = marked(props.article_content);
 
-						</div>
+  return (
+    <>
+      <Head>
+        <title>博客详细页</title>
+      </Head>
+      <Header />
+      <Row className="comm-main" type="flex" justify="center">
+        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+          <div>
+            <div className="bread-div">
+              <Breadcrumb>
+                <Breadcrumb.Item>
+                  <a href="/">首页</a>
+                </Breadcrumb.Item>
+                <Breadcrumb.Item>视频列表</Breadcrumb.Item>
+                <Breadcrumb.Item>xxxx</Breadcrumb.Item>
+              </Breadcrumb>
+            </div>
 
-					</div>
-				</Col>
+            <div>
+              <div className="detailed-title">React实战视频教程-(更新08集)</div>
 
-				<Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-					<Author />
-					<Advert />
-					<Affix offsetTop={5}>
-						<div className="detailed-nav comm-box">
-							<div className="nav-title">文章目录</div>
-							<MarkNav
-								className="article-menu"
-								source={markdown}
-								ordered={false} 
-							/>
-						</div>
-					</Affix>
-				</Col>
-			</Row>
-			<Footer />
+              <div className="list-icon center">
+                <span>
+                  <Icon type="calendar" /> 2019-06-28
+                </span>
+                <span>
+                  <Icon type="folder" /> 视频教程
+                </span>
+                <span>
+                  <Icon type="fire" /> 5498人
+                </span>
+              </div>
 
-		</>
-	)
-}
+              <div className="detailed-content" dangerouslySetInnerHTML={{ __html: html }}></div>
+            </div>
+          </div>
+        </Col>
 
-export default Detailed
+        <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+          <Author />
+          <Advert />
+          <Affix offsetTop={5}>
+            <div className="detailed-nav comm-box">
+              <div className="nav-title">文章目录</div>
+              {/* <MarkNav className="article-menu" source={html} ordered={false} /> */}
+              {tocify && tocify.render()}
+            </div>
+          </Affix>
+        </Col>
+      </Row>
+      <Footer />
+    </>
+  );
+};
+Detailed.getInitialProps = async context => {
+  console.log(context.query.id);
+  let id = context.query.id;
+  const promise = new Promise(resolve => {
+    axios(servicePath.getArticleById + id).then(res => {
+      resolve(res.data.data[0]);
+    });
+  });
+
+  return await promise;
+};
+export default Detailed;
